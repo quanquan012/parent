d239aecb2a7228d885b49d824aa621d6883ea486
lihao <975571839@qq.com>
2018-11-29 16:02:31 +0800

    base-1.0

--- service-zuul/src/main/java/com/config/FilterBeans.java
+++ service-zuul/src/main/java/com/config/FilterBeans.java
@@ -1,8 +1,6 @@
 package com.config;
 
 import com.filter.TokenFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author: li.hao
